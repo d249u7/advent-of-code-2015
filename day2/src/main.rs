381910b5e0f2a03bd3836f70e2c55f0cfee4a916
Dragut <d249u7@outlook.com>
2022-12-14 00:53:03 +0300

    day2

--- day2/src/main.rs
+++ day2/src/main.rs
@@ -1,8 +1,8 @@
-use std::fs;
+use std::{fs, path::Path};
 
 fn main() {
-    let input = fs::read_to_string("/home/dragut/Projects/advent-of-code-2015/day2/input.txt")
-        .expect("File could not be read");
+    let path = Path::new("input.txt");
+    let input = fs::read_to_string(path).expect("File could not be read");
 
     let lines = input.lines();
 
@@ -14,22 +14,19 @@ fn main() {
             .map(|c| c.parse::<i32>().unwrap())
             .collect::<Vec<i32>>();
 
-        fn calculate_areas(dimensions: Vec<i32>) -> Vec<i32> {
-            let multiple_all: i32 = dimensions.iter().product();
+        fn calculate_areas(dimensions: &[i32]) -> Vec<i32> {
+            let sum_all: i32 = dimensions.iter().sum();
 
-            dimensions.iter().map(|d| multiple_all / d).collect()
+            dimensions.iter().map(|d| 2 * (sum_all - d)).collect()
         }
 
-        let dimensions = calculate_areas(side_dimensions);
+        let dimensions = calculate_areas(&side_dimensions);
 
         let minimum = dimensions.iter().min().unwrap();
 
-        let area_sum = dimensions.iter().fold(0, |mut acc, area| {
-            acc += 2 * area;
-            acc
-        });
+        let volume = side_dimensions.iter().product::<i32>();
 
-        total += area_sum + minimum;
+        total += volume + minimum;
     }
     println!("{}", total);
 }
