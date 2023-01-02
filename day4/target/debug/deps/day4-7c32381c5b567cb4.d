3670b8e5c4533f2aab91ac66f86a0abefd6d1d23
Dragut <d249u7@outlook.com>
2023-01-02 22:57:56 +0000

    hmm

--- day4/target/debug/deps/day4-7c32381c5b567cb4.d
+++
@@ -1,5 +0,0 @@
-/home/dragut/Projects/advent-of-code-2015/day4/target/debug/deps/day4-7c32381c5b567cb4.rmeta: src/main.rs
-
-/home/dragut/Projects/advent-of-code-2015/day4/target/debug/deps/day4-7c32381c5b567cb4.d: src/main.rs
-
-src/main.rs:
